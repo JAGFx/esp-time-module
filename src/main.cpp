6fbcc21971a572254482492bd3001e688776c1af
JAGFx <contact@jagfx.fr>
2021-08-24 20:55:04 +0200

    :bug: split time string

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,8 @@
 #define clkPin     D7
 #define csPin      D6
 #define numDevices  1
-LedControl    lc         = LedControl( dinPin, clkPin, csPin, numDevices );
-//const byte    BRIGHTNESS = 2;
-unsigned long delaytime  = 250;
+LedControl    lc        = LedControl( dinPin, clkPin, csPin, numDevices );
+unsigned long delaytime = 250;
 
 char h[2];
 char m[2];
@@ -24,65 +23,7 @@ char s[2];
 // -- Wifi
 #define WIFI_MAX_TRY 10
 
-IPAddress ip( 192, 168, 1, 75 );
-IPAddress gateway( 192, 168, 1, 1 );
-IPAddress subnet( 255, 255, 255, 0 );
-uint8_t   wifiCounterTry = 0;
-
-// -- NTPClient
-//#define SHOW_TIME_PERIOD 1000
-#define ONBOARDLED 2 // Built in LED on ESP-12/ESP-07
-#define SHOW_TIME_PERIOD 5000
-#define NTP_TIMEOUT 1500
-
-int8_t             timeZone           = 1;
-int8_t             minutesTimeZone    = 0;
-const PROGMEM char *ntpServer         = "pool.ntp.org";
-bool               wifiFirstConnected = false;
-
-void onSTAConnected( WiFiEventStationModeConnected ipInfo ) {
-    Serial.printf( "Connected to %s\r\n", ipInfo.ssid.c_str() );
-}
-
-
-// Start NTP only after IP network is connected
-void onSTAGotIP( WiFiEventStationModeGotIP ipInfo ) {
-    Serial.printf( "Got IP: %s\r\n", ipInfo.ip.toString().c_str() );
-    Serial.printf( "Connected: %s\r\n", WiFi.status() == WL_CONNECTED ? "yes" : "no" );
-    digitalWrite( ONBOARDLED, LOW ); // Turn on LED
-    wifiFirstConnected = true;
-}
-
-// Manage network disconnection
-void onSTADisconnected( WiFiEventStationModeDisconnected event_info ) {
-    Serial.printf( "Disconnected from SSID: %s\n", event_info.ssid.c_str() );
-    Serial.printf( "Reason: %d\n", event_info.reason );
-    digitalWrite( ONBOARDLED, HIGH ); // Turn off LED
-    //NTP.stop(); // NTP sync can be disabled to avoid sync errors
-    WiFi.reconnect();
-}
-
-void processSyncEvent( NTPSyncEvent_t ntpEvent ) {
-    if ( ntpEvent < 0 ) {
-        Serial.printf( "Time Sync error: %d\n", ntpEvent );
-        if ( ntpEvent == noResponse )
-            Serial.println( "NTP server not reachable" );
-        else if ( ntpEvent == invalidAddress )
-            Serial.println( "Invalid NTP server address" );
-        else if ( ntpEvent == errorSending )
-            Serial.println( "Error sending request" );
-        else if ( ntpEvent == responseError )
-            Serial.println( "NTP response error" );
-    } else {
-        if ( ntpEvent == timeSyncd ) {
-            Serial.print( "Got NTP time: " );
-            Serial.println( NTP.getTimeDateString( NTP.getLastNTPSync() ) );
-        }
-    }
-}
-
-boolean        syncEventTriggered = false; // True if a time even has been triggered
-NTPSyncEvent_t ntpEvent; // Last triggered event
+uint8_t wifiCounterTry = 0;
 
 // ---- ./Modules
 // ----------------------
@@ -112,8 +53,6 @@ void writeSecond() {
 // ----- Minimal functions
 
 void setup() {
-    static WiFiEventHandler e1, e2, e3;
-    
     Serial.begin( 115200 );
     
     // --- LedDisplay
@@ -128,18 +67,12 @@ void setup() {
     lc.setChar( 0, 6, '-', false );
     lc.setChar( 0, 7, '-', false );
     
-    delay( delaytime );
-    
-    
+    delay( 100 );
     
     // --- Wifi
     Serial.println( "Wifi init..." );
-    WiFi.config( ip, gateway, subnet );
     WiFi.mode( WIFI_STA );
     WiFi.begin( WIFI_SSID, WIFI_PSWD );
-    
-    pinMode( ONBOARDLED, OUTPUT ); // Onboard LED
-    digitalWrite( ONBOARDLED, HIGH ); // Switch off LED
     
     while ( WiFi.status() != WL_CONNECTED ) {
         Serial.print( "Wifi connecting #" );
@@ -160,45 +93,16 @@ void setup() {
     delay( 1000 );
     
     // --- Ntp client
-//    NTP.setInterval (63);
-//    NTP.setNTPTimeout (1000);
     NTP.begin();
     NTP.setTimeZone( 1 );
     NTP.setDayLight( true );
     int counter = 0;
     
-    while ( !NTP.SyncStatus()) {
-        Serial.print("NTP CHECK: #");
-        Serial.println(++counter);
-        
-        if( counter > 10 ){
-            counter = 0;
-            NTP.stop();
-            NTP.begin();
-            NTP.setTimeZone( 1 );
-            NTP.setDayLight( true );
-            Serial.println( "NTP: Reset" );
-        }
-        
-        delay(500);
-    };
-    
-    
-//    NTP.begin();
-    
-//    NTP.onNTPSyncEvent( []( NTPSyncEvent_t event ) {
-//        ntpEvent           = event;
-//        syncEventTriggered = true;
-//    } );
-//
-//    e1 = WiFi.onStationModeGotIP( onSTAGotIP );// As soon WiFi is connected, start NTP Client
-//    e2 = WiFi.onStationModeDisconnected( onSTADisconnected );
-//    e3 = WiFi.onStationModeConnected( onSTAConnected );
-    
-    lc.clearDisplay( 0 );
-    
     // ---
+    lc.clearDisplay( 0 );
 }
+
+const static char *deleimiter = ":";
 
 void loop() {
     // --- Wifi connexion
@@ -206,58 +110,24 @@ void loop() {
         ESP.restart();
     
     // --- Ntp client
-    static int i    = 0;
-    static int last = 0;
+    lc.clearDisplay( 0 );
+    String time = NTP.getTimeStr();
+    Serial.println( time );
     
-//    if ( wifiFirstConnected ) {
-//        wifiFirstConnected = false;
-//        NTP.setInterval( 63 );
-//        NTP.setNTPTimeout( NTP_TIMEOUT );
-//        NTP.begin( ntpServer, timeZone, true, minutesTimeZone );
-//    }
+    sprintf( s, "%s", time.substring( 6, 8 ).c_str() );
+    sprintf( m, "%s", time.substring( 3, 5 ).c_str() );
+    sprintf( h, "%s", time.substring( 0, 2 ).c_str() );
     
-//    if ( syncEventTriggered ) {
-//        processSyncEvent( ntpEvent );
-//        syncEventTriggered = false;
-//    }
+    writeHour();
+    writeMinute();
+    writeSecond();
     
-    if ( ( millis() - last ) > SHOW_TIME_PERIOD ) {
-
-
-//    if ( ( millis() - last ) >= SHOW_TIME_PERIOD ) {
-        // --- LedDisplay
-//        lc.clearDisplay( 0 );
-        
-        last = millis();
-        Serial.print( i );
-        Serial.print( " " );
-        Serial.print( NTP.getTimeDateString() );
-        Serial.print( " " );
-        Serial.print( NTP.isSummerTime() ? "Summer Time. " : "Winter Time. " );
-        Serial.print( "WiFi is " );
-        Serial.print( WiFi.isConnected() ? "connected" : "not connected" );
-        Serial.print( ". " );
-        Serial.print( "Uptime: " );
-        Serial.print( NTP.getUptimeString() );
-        Serial.print( " since " );
-        Serial.println( NTP.getTimeDateString( NTP.getFirstSync() ).c_str() );
-        Serial.printf( "Free heap: %u\n", ESP.getFreeHeap() );
-        i++;
-//        sprintf( h, "%02d", timeNtp.getHours() );
-//        sprintf( m, "%02d", timeNtp.getMinutes() );
-//        sprintf( s, "%02d", timeNtp.getSeconds() );
-
-//        writeHour();
-//        writeMinute();
-//        writeSecond();
-    }
+    Serial.print( h );
+    Serial.print( 'h' );
+    Serial.print( m );
+    Serial.print( 'm' );
+    Serial.print( s );
+    Serial.println( 's' );
     
-//    Serial.print( h );
-//    Serial.print( 'h' );
-//    Serial.print( m );
-//    Serial.print( 'm' );
-//    Serial.print( s );
-//    Serial.println( 's' );
-    
-    delay (0);
+    delay( 1000 );
 }
